3a04fbf4403412484abc1863a935fb72521b7556
Equirizon <equirizon@gmail.com>
2025-02-07 19:27:27 +0800

    added word filter

--- script.js
+++ script.js
@@ -8,25 +8,28 @@ button.addEventListener('click', () => {
     });
 
 const palindromeEngine = (word) => {
-    let arr = Array.from(word).reverse();
+    console.log('-- start --');
+    const regEx = /[^a-zA-Z]/ig
+    const filter = word.replace(regEx, '');
+    const arr = Array.from(filter).reverse();
     let reversed = '';
 
-    console.log(`Reversed array: ${arr}`)
+    console.log(`RegEx: '${word.match(regEx)}'`);
+    console.log(`Reversed array: [${arr}]`)
 
     for (const letters of arr) {
         reversed += letters;
     }
 
-    console.log(`Word: "${word}"`);
-    console.log(`Reversed word "${reversed}"`);
+    console.log(`Word: '${filter}'`);
+    console.log(`Reversed word: '${reversed}'`);
 
-    if (word.length == 1 || word === reversed) {
-        output.textContent = `${word} is a palindrome!`;
+    if (word.length == 1 || filter === reversed) {
+        output.textContent = `"${word}" is a palindrome!`;
     }    
 
     else {
-        output.textContent = `${word} is NOT a palindrome!`;
-
+        output.textContent = `"${word}" is NOT a palindrome!`;
     }
 }
 
