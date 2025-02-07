0b91fb09419a0e219173b977b005cf820ec5097f
Equirizon <equirizon@gmail.com>
2025-02-07 20:13:53 +0800

    removed prints

--- script.js
+++ script.js
@@ -8,30 +8,20 @@ button.addEventListener('click', () => {
     });
 
 const palindromeEngine = (word) => {
-    console.log('-- start --');
     const regEx = /[^a-z]/ig;
     const filter = word.replace(regEx, '').toLowerCase(); //removes non-letter characters
     const arr = Array.from(filter).reverse();
     let reversed = '';
 
-    console.log(`RegEx: '${word.match(regEx)}'`);
-
     for (const letters of arr) {
         reversed += letters;
     }
 
-    console.log(`Word: '${filter}'`);
-    console.log(`Reversed word: '${reversed}'`);
-
     if (word.length == 1 || filter === reversed.toLowerCase()) { //checks if word is a palindrome
         output.textContent = `"${word}" is a palindrome!`;
-        console.log("Palindrome: True");
     }    
 
     else {
         output.textContent = `"${word}" is NOT a palindrome!`;
-        console.log("Palindrome: False");
     }
 }
-
-console.log("done!");
